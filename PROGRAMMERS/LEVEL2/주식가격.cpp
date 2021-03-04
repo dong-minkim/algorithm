b8da47417684426524f5c93ff3877cf13264b764
Dongmin Kim <kimdongmin68@naver.com>
2021-03-04 18:44:51 +0900

    Level2

---
+++ PROGRAMMERS/LEVEL2/주식가격.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> prices) {
+    vector<int> answer;
+    int cnt;
+    for (int i = 0; i < prices.size(); i++) {
+        cnt = 0;
+        for (int j = i + 1; j < prices.size(); j++) {
+            if (prices[i] <= prices[j]) cnt++;
+            else { cnt++; break; }
+        }
+        answer.push_back(cnt);
+    }
+    return answer;
+}
