0f7aa455c718475750369c7e8ac61dd865f2e690
Dongmin Kim <kimdongmin68@naver.com>
2021-03-06 14:43:30 +0900

    Level1

---
+++ PROGRAMMERS/LEVEL1/문자열 다루기 기본.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+bool solution(string s) {
+    bool answer = true;
+    if (s.length() == 4 || s.length() == 6) {
+        for (int i = 0; i < s.length(); i++) {
+            if (!('0' <= s[i] && s[i] <= '9'))
+                return false;
+        }
+    }
+    else return false;
+
+    return answer;
+}
