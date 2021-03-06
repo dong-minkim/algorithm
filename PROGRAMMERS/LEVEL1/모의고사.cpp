00334789304b87f83e0da860f91106df19afcd56
Dongmin Kim <kimdongmin68@naver.com>
2021-03-06 14:43:06 +0900

    Level1

---
+++ PROGRAMMERS/LEVEL1/모의고사.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <vector>
+#include<algorithm>
+
+using namespace std;
+
+int a[5] = { 1,2,3,4,5 };
+int b[8] = { 2,1,2,3,2,4,2,5 };
+int c[10] = { 3,3,1,1,2,2,4,4,5,5 };
+vector<int> solution(vector<int> answers) {
+    vector<int> answer;
+    int score[3] = { 0, };
+    for (int i = 0; i < answers.size(); i++) {
+        if (answers[i] == a[i % 5]) score[0] += 1;
+        if (answers[i] == b[i % 8]) score[1] += 1;
+        if (answers[i] == c[i % 10]) score[2] += 1;
+    }
+    int max_ = 0;
+    max_ = max(score[0], max(score[1], score[2]));
+    for (int i = 0; i < 3; i++) {
+        if (max_ == score[i]) answer.push_back(i + 1);
+    }
+
+    return answer;
+}
