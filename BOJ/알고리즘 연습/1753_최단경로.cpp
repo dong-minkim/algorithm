4758709de4444ac3b44e461fe2006d03b22a1b37
Dongmin Kim <kimdongmin68@naver.com>
2021-03-24 19:01:02 +0900

    BOJ

---
+++ BOJ/알고리즘 연습/1753_최단경로.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+#include<vector>
+#include<queue> // 우선순위 큐 사용하기 위함
+
+using namespace std;
+
+int V, E;
+int K;
+int u, v, w;
+priority_queue<pair<int, int>> pq;
+vector<pair<int, int>> e[20001]; //간선 정보
+int d[20001] = { 0, };//최단거리
+
+void dijkstra(int node) {
+
+
+	d[node] = 0; //시작하는 노드는 최단거리 0
+	pq.push(make_pair(0, node));
+
+	while (!pq.empty()) {
+		int distance = -pq.top().first;
+		int current = pq.top().second;//pq.top().second는 음수이다 (top에서 가장 최단거리를 뽑기위해)
+		pq.pop();
+		if (d[current] < distance) continue;
+		for (int i = 0; i < e[current].size(); i++) {
+			int next = e[current][i].first;
+			int next_distance = distance + e[current][i].second;
+			if (next_distance < d[next]) {
+				d[next] = next_distance;
+				pq.push(make_pair(-next_distance, next));
+			}
+		}
+	}
+}
+
+int main()
+{
+	cin >> V >> E;
+	cin >> K;
+	for (int i = 1; i <= V; i++) {
+		d[i] = 1000000;
+	}
+	for (int i = 0; i < E; i++) {
+		cin >> u >> v >> w;
+		e[u].push_back(make_pair(v, w)); //u에서 v 정점 가중치는 w
+	}
+
+	dijkstra(K);
+
+	for (int i = 1; i <= V; i++) {
+		if (d[i] == 1000000) cout << "INF" << '\n';
+		else {
+			cout << d[i] << '\n';
+		}
+	}
+
+	return 0;
+}
