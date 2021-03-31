e0c7733159c15d3e7a60f528916cb1d588acf5ea
Dongmin Kim <kimdongmin68@naver.com>
2021-04-01 02:45:21 +0900

    Level1

--- PROGRAMMERS/LEVEL1/[1차] 비밀지도.cpp
+++ PROGRAMMERS/LEVEL1/[1차] 비밀지도.cpp
@@ -1,21 +1,22 @@
-#include<iostream>
-#include<vector>
-#include<algorithm>
-#include<queue>
-#include<cstring>
+#include <string>
+#include <vector>
 
 using namespace std;
 
-int main()
-{
-	int t;
-	int a, b;
-	cin >> t;
-	while (t--) {
-		cin >> a >> b;
-		int tmp;
-		tmp = a | b;
-		cout << tmp << '\n';
-	}
-	return 0;
+
+vector<string> solution(int n, vector<int> arr1, vector<int> arr2) {
+    vector<string> answer;
+    string st;
+    for (int i = 0; i < n; i++) {
+        int num = arr1[i] | arr2[i];
+        st = "";
+        for (int j = 0; j < n; j++) {
+            if (num % 2 == 0) st = " " + st;
+            else st = "#" + st;
+            num /= 2;
+        }
+        answer.push_back(st);
+    }
+
+    return answer;
 }
