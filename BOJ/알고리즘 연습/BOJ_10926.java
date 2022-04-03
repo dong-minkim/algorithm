02f3a985327db888b4a0f26ffcfb9e5e0921d855
Dongmin Kim <kimdongmin68@naver.com>
2022-04-04 00:10:03 +0900

    ??!

---
+++ BOJ/알고리즘 연습/BOJ_10926.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class BOJ_10926 {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		String str;
+		
+		str = scan.nextLine();
+		
+		System.out.println(str+"??!");
+
+	}
+
+}
