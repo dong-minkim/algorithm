43691d89d1e305dd2b2bb1774202ed065e5fc540
Dongmin Kim <kimdongmin68@naver.com>
2022-04-03 23:47:44 +0900

    A*B

---
+++ BOJ/알고리즘 연습/BOJ_10998.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class BOJ_10998 {
+
+	public static void main(String[] args) {
+
+		int A;
+		int B;
+		
+		Scanner scan = new Scanner(System.in);
+		
+		A = scan.nextInt();
+		B = scan.nextInt();
+		
+		System.out.println(A-B);
+		
+		scan.close();
+
+	}
+
+}
