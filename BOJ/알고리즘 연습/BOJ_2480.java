da6429f6311d25492a70c581381908044ac7ec33
Dongmin Kim <kimdongmin68@naver.com>
2022-04-08 05:09:08 +0900

    주사위 세개

---
+++ BOJ/알고리즘 연습/BOJ_2480.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class BOJ_2480 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		
+		if(a==b && a==c && b==c) {
+			System.out.println(a*1000 + 10000);
+		}
+		else if(a==b || a==c) {
+			System.out.println(a*100 + 1000);
+		}
+		else if(b==c) {
+			System.out.println(b*100+1000);
+		}
+		else {
+			System.out.println(100 * Math.max(a, Math.max(b, c)));
+		}
+	}
+
+}
