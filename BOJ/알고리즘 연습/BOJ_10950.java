3ec3fd6df5ff851f2110f2d936039a3e1b735f15
Dongmin Kim <kimdongmin68@naver.com>
2022-04-19 00:15:19 +0900

    A+B - 3

---
+++ BOJ/알고리즘 연습/BOJ_10950.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class BOJ_10950 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int testcase = sc.nextInt();
+		
+		for(int i=0;i<testcase;i++) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			System.out.println(a+b);
+		}
+	}
+
+}
