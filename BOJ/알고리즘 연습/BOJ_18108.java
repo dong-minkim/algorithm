6629d3ed8bbcd41a4130ffa7710891f76d20116b
Dongmin Kim <kimdongmin68@naver.com>
2022-04-22 21:39:37 +0900

    1998년생인 내가 태국에서는 2541년생?!

---
+++ BOJ/알고리즘 연습/BOJ_18108.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class BOJ_18108 {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int y = scan.nextInt();
+		
+		System.out.println(y-543);
+		
+	}
+
+}
