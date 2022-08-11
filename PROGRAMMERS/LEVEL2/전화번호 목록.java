4c524590b4e6b76da13013c63d2a1ba0efa02bff
Dongmin Kim <kimdongmin68@naver.com>
2022-08-12 03:42:31 +0900

    Level2

---
+++ PROGRAMMERS/LEVEL2/전화번호 목록.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.util.Map.Entry;
+
+class Solution {
+	public boolean solution(String[] phone_book) {
+		boolean answer = true;
+
+		Arrays.sort(phone_book);
+
+		for (int i = 0; i < phone_book.length - 1; i++) {
+			if(phone_book[i + 1].startsWith(phone_book[i]))
+                return false;
+		}
+
+		return answer;
+	}
+}
