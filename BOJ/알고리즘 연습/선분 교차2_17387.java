9d1f1cc0309a4237795d357dd4f173d37f40dc47
Dongmin Kim <kimdongmin68@naver.com>
2022-12-09 13:26:35 +0900

    CCW (세 점 방향)

---
+++ BOJ/알고리즘 연습/선분 교차2_17387.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Point a = new Point(sc.nextLong(), sc.nextLong());
+        Point b = new Point(sc.nextLong(), sc.nextLong());
+        Point c = new Point(sc.nextLong(), sc.nextLong());
+        Point d = new Point(sc.nextLong(), sc.nextLong());
+
+        int result1 = ccw(a, b, c) * ccw(a, b, d);
+        int result2 = ccw(c, d, a) * ccw(c, d ,b);
+
+        if(result1 == 0 && result2 == 0) { //일직선인 경우
+            if(Math.min(a.x, b.x) <= Math.max(c.x, d.x) && Math.min(c.x, d.x) <= Math.max(a.x, b.x) &&
+                    Math.min(a.y, b.y) <= Math.max(c.y, d.y) && Math.min(c.y, d.y) <= Math.max(a.y, b.y)) {
+                System.out.println(1);
+            } else {
+                System.out.println(0);
+            }
+        } else if(result1 <= 0 && result2 <= 0) {
+            System.out.println(1);
+        } else {
+            System.out.println(0);
+        }
+
+    }
+
+    static int ccw(Point a, Point b, Point c) {
+        long result = ( a.x * b.y + b.x * c.y + c.x * a.y) - ( a.x * c.y + c.x * b.y + b.x * a.y);
+        if(result == 0) return 0;
+        else if(result > 0) return 1;
+        else return -1;
+    }
+
+    static class Point {
+        long x, y;
+
+        public Point(long x, long y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
